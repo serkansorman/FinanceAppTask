2ccd853673be5d9170156455f22e6177d9426684
serkansorman <serkan.sorman@getir.com>
2022-06-15 01:12:12 +0300

    Stock description UI and response data changed, unused dependencies removed

--- app/src/main/java/com/example/ccfinancegrouptask/data/model/response/StockDescriptionResponseModel.kt
+++ app/src/main/java/com/example/ccfinancegrouptask/data/model/response/StockDescriptionResponseModel.kt
@@ -2,17 +2,21 @@ package com.example.ccfinancegrouptask.data.model.response
 
 import com.google.gson.annotations.SerializedName
 
+
 data class StockDescriptionResponseModel(
     @SerializedName("price")
-    val data: Data?
+    val summary: Summary?
 )
 
-data class Data(
-    val exchange: String?,
+data class Summary(
     val shortName: String?,
-    val longName: String?,
-    val exchangeName: String?,
-    val currency: String?
+    val currency: String?,
+    val regularMarketPrice: RegularMarketPrice?,
+    val regularMarketChangePercent: RegularMarketPrice?,
+    val regularMarketOpen: RegularMarketPrice?,
+    val regularMarketPreviousClose: RegularMarketPrice?,
+    val regularMarketDayLow: RegularMarketPrice?,
+    val regularMarketDayHigh: RegularMarketPrice?
 )
 
-
+data class RegularMarketPrice(val raw: String?, val fmt: String?)
--- app/src/main/java/com/example/ccfinancegrouptask/ui/stockdescription/fragment/StockDescriptionFragment.kt
+++ app/src/main/java/com/example/ccfinancegrouptask/ui/stockdescription/fragment/StockDescriptionFragment.kt
@@ -8,9 +8,9 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
 import com.example.ccfinancegrouptask.base.BaseFragment
+import com.example.ccfinancegrouptask.data.model.response.StockDescriptionResponseModel
 import com.example.ccfinancegrouptask.databinding.FragmentStockDescriptionBinding
 import com.example.ccfinancegrouptask.ui.stockdescription.event.StockDescriptionEvent
-import com.example.ccfinancegrouptask.ui.stocklist.event.StockListEvent
 import com.example.ccfinancegrouptask.ui.stocklist.viewmodel.StockListViewModel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -31,19 +31,28 @@ class StockDescriptionFragment : BaseFragment() {
 
     }
 
+    fun initDetailUI(stockDescription: StockDescriptionResponseModel) {
+        binding.apply { //TODO change rate color
+            stockDescription.summary?.apply {
+                textStockName.text = shortName
+                textStockCurrency.text = currency
+                textStockPrice.text = regularMarketPrice?.fmt
+                textStockChangeRate.text = regularMarketChangePercent?.fmt
+                textStockOpenPrice.text = regularMarketOpen?.fmt
+                textStockClosePrice.text = regularMarketPreviousClose?.fmt
+                textStockLow.text = regularMarketDayLow?.fmt
+                textStockHigh.text = regularMarketDayHigh?.fmt
+            }
+        }
+    }
+
     override fun initObservers() {
         lifecycleScope.launch{
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED){
                 viewModel.stockDescriptionFlow.collect{
                     when(it){
                         is StockDescriptionEvent.Success -> {
-                            binding.apply {
-                                symbol.text = it.stockDescription.data?.shortName
-                                shortName.text = it.stockDescription.data?.longName
-                                longName.text = it.stockDescription.data?.exchangeName
-                                price.text = it.stockDescription.data?.currency
-
-                            }
+                            initDetailUI(it.stockDescription)
                         }
                         is StockDescriptionEvent.Failure -> {
                             //TODO handle error
